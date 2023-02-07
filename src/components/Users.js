94e4bc289dbaa9ded3705754381946d745b2230a
AMINA-S-IBRAHIM <amina_ibrahim@purplebeard.co.uk>
2023-02-07 22:24:08 +0000

    modified:   src/App.js 	deleted:    src/components/AddUserForm.js 	deleted:    src/components/ConfirmationModal.js 	new file:   src/components/Form.js 	renamed:    src/pages/Home.js -> src/components/Home.js 	renamed:    src/pages/NavigationBar.js -> src/components/NavigationBar.js 	renamed:    src/pages/Outcomes.js -> src/components/Outcomes.js 	renamed:    src/pages/Request.js -> src/components/Requests.js 	deleted:    src/components/SuccessModal.js 	deleted:    src/components/UserCard.js 	modified:   src/components/Users.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/pages/BannerCard.js 	deleted:    src/pages/Users.js 	deleted:    src/utils/getFromLocalStorage.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,34 +1,10 @@
-import Alert from "react-bootstrap/Alert";
-import Stack from "react-bootstrap/Stack";
-import { UserCard } from "./UserCard";
+import React from 'react';
 
-export const Users = ({
-  users,
-  setCurrentUser,
-  handleConfirmationModalOpen,
-}) => {
-  if (users.length === 0) {
-    return (
-      <Alert variant="info">
-        You have no users in the registry. Please complete the form to add a new
-        user.
-      </Alert>
+
+const Users= ()=>{
+    return(
+        <div className="App">Users route</div>
     );
-  }
-
-  return (
-    <Stack
-      direction="horizontal"
-      className="justify-content-between align-items-start"
-    >
-      {users.map((user) => (
-        <UserCard
-          user={user}
-          key={user.id}
-          setCurrentUser={setCurrentUser}
-          handleConfirmationModalOpen={handleConfirmationModalOpen}
-        />
-      ))}
-    </Stack>
-  );
 };
+
+export default Users;
--- src/utils/getFromLocalStorage.js
+++
@@ -1,9 +0,0 @@
-export const getDataFromLocalStorage = (key, defaultValue) => {
-  const dataFromLocalStorage = JSON.parse(localStorage.getItem(key));
-
-  if (!dataFromLocalStorage) {
-    return defaultValue;
-  }
-
-  return dataFromLocalStorage;
-};
