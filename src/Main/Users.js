b0ae0fd1915bbc84e82e0c79b2dae0507a41e3ac
AMINA-S-IBRAHIM <amina_ibrahim@purplebeard.co.uk>
2023-02-09 01:08:51 +0000

    modified:   src/Main/Users.js 	deleted:    src/components/Add.js 	deleted:    src/components/Edit.js 	deleted:    src/components/Home.js 	deleted:    src/components/Users.js 	deleted:    src/components/View.js

--- src/Main/Users.js
+++ src/Main/Users.js
@@ -1,23 +1,13 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Add from '../components/Add';
-import Edit from '../components/Edit';
-import Home from '../components/Home';
 
-
-function App() {
+import React from 'react';
+import Form from'./Form';
+function Users() {
   return (
     <div className="App">
-      <Home />
-      <Router>
-        <Routes>
-            
-            <Route path="/create" element={<Add />} />
-            <Route path="/Edit" element={<Edit/>} />
-        </Routes>
-        
-      </Router>
+    <Form/>
+      
     </div>
   );
 }
 
-export default App;
+export default Users;
